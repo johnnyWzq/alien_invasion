a2dd4aad9c49cfe8c802a69cb1dfee0095044d8f
JohnnyWzq <vict_cfy@163.com>
2018-03-08 15:15:29 +0800

    creat a new project

---
+++ bullet.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  8 14:54:45 2018
+
+@author: Admin
+"""
+
---
+++ setting.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  8 10:48:41 2018
+
+@author: wuzhiqiang
+"""
+
+class Settings():
+    """存储项目的所有设置的类"""
+    
+    def __init__(self):
+        """初始化"""
+        #屏幕设置
+        self.screen_width = 800
+        self.screen_heigh = 600
+        self.bg_color = (230, 230, 230)
+        
+        #飞船的设置
+        self.ship_speed_factor = 1.5
+        
+        #子弹设置
+        self.bullet_speed_factor = 1
+        self.bullet.width = 3
+        self.bullet.height = 15
+        self.bullet_color = 60, 60, 60
