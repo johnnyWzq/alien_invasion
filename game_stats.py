7c14ea74eb731632e9dd31718ef29ccdd78b231c
JohnnyWzq <vict_cfy@163.com>
2018-03-16 13:25:44 +0800

    add stats

---
+++ game_stats.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 16 12:53:14 2018
+
+@author: wuzhiqiang
+"""
+
+class GameStats():
+    """跟踪游戏的统计信息"""
+    
+    def __init__(self, ai_settings):
+        """初始化统计信息"""
+        self.ai_settings = ai_settings
+        self.reset_stats()
+        #游戏刚启动时处于活动状态
+        self.game_active = True
+        
+    def reset_stats(self):
+        """初始化在游戏运行期间可能变化的统计信息"""
+        self.ships_left = self.ai_settings.ship_limit
